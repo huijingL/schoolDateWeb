7379af2cd90d498789f93a0542271ae90007f878
huijingL <38586385+huijingL@users.noreply.github.com>
2019-03-30 19:48:51 +0800

    已完成发送http请求,待解决router问题

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import 'antd/dist/antd.css';
 import Routers from './router/index';
 import * as serviceWorker from './serviceWorker';
+import config from './config/index';
+import axios from './http/axios'
+
+React.Component.prototype.$config = config;
+React.Component.prototype.$http = axios;
+
 
 ReactDOM.render((
     <Routers />
--- src/layout/login.js
+++ src/layout/login.js
@@ -4,24 +4,73 @@ import './login.css'
 
 class Login extends React.Component {
     constructor(props){
-        super()
+        super(props);
+        this.state={
+            userName: 'liuyanwei',
+            password: '',
+            checkPassword: '',
+            isLogin: true,
+        }
+    }
+    dealInput = (e)=>{
+        e.persist()
+        console.log(e);
+        this.setState(
+            {
+                [e.target.id]: e.target.value,
+            }
+        )
+    };
+    dealClick(e){
+        e.persist()
+        let methods = {
+            login: function(){
+                console.log("login");
+            },
+            regist: ()=>{
+                if(this.state.checkPassword&&this.state.password&&this.state.checkPassword===this.state.password){
+                    this.regist()
+                }else{
+                    console.log('error')
+                }
+            }
+        }
+        methods[e.target.id]&&methods[e.target.id]();
+    };
+
+    async regist(){
+        console.log(this.$config.host);
+        // let data = await this.$http.get(this.$config.host+this.$config.api.schema+"/bd_customer/",{params:{filter:{__s:0}}})
+        let data = await this.$http.get('http://127.0.0.1:5000')
+        console.log(data);
     }
     render(){
         return (
             <div className="loginBackground">
-                <Card className="loginCard"
-                      title={<span style={{color: 'aliceblue',}}>用户登录</span>}
-                    extra={<a href="#">More</a>}
-                    style={{ width: 400 }}
-                >
-                    <p><Input addonBefore="用户名：" defaultValue="" /></p>
-                    <p><Input addonBefore="密码：" type="password" defaultValue="" /></p>
-                    <p><Button type="primary" block>登录</Button></p>
+                {this.state.isLogin ? ( 
+                < Card className = "loginCard"
+                    title={<span style={{color: 'aliceblue',}}>用户登录</span>}
+                    extra={<a onClick={()=>{this.setState({isLogin: !(this.state.isLogin)})}}>More</a>}
+                    style={{ width: 400 }}>
+                    <p><Input addonBefore="用户名：" id="userName" onChange={this.dealInput} value={this.state.userName}/></p>
+                    <p><Input.Password  addonBefore="密码：" id="password" onChange={this.dealInput} value={this.state.password} /></p>
+                    <p><Button type="primary" id="login" onClick={this.dealClick.bind(this)}  block>登录</Button></p>
                 </Card>
+                ):(
+                    < Card className = "loginCard"
+                      title={<span style={{color: 'aliceblue',}}>用户注册</span>}
+                    extra={<a onClick={()=>{this.setState({isLogin: !(this.state.isLogin)})}}>More</a>}
+                    style={{ width: 400 }}>
+                    <p><Input addonBefore="用户名：" id="userName" onChange={this.dealInput} value={this.state.userName}/></p>
+                    <p><Input.Password  addonBefore="密码："  id="password" onChange={this.dealInput} value={this.state.password} /></p>
+                    <p><Input.Password  addonBefore="确认密码："  id="checkPassword" onChange={this.dealInput} value={this.state.checkPassword} /></p>
+                    <p><Button type="primary" id="regist" onClick={this.dealClick.bind(this)}  block>注册</Button></p>
+                </Card>
+                )
+                }
             </div>
         )
     }
-
 }
 
 export default Login;
